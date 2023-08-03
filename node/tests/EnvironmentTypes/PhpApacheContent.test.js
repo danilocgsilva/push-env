21271f0b387c4be6c29430d4fcf1f086d9e88975
Danilo Silva <danilocgsilva@gmail.com>
2023-08-03 17:04:40 -0300

    Fix Dockerfile for multiline

--- node/tests/EnvironmentTypes/PhpApacheContent.test.js
+++ node/tests/EnvironmentTypes/PhpApacheContent.test.js
@@ -101,9 +101,9 @@ services:
 RUN apt-get update
 RUN apt-get install vim zip -y
 RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin/ --filename=composer
-RUN pecl install xdebug && docker-php-ext-enable xdebug &&
-  echo "" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini &&
-  echo "xdebug.mode = debug" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini &&
+RUN pecl install xdebug && docker-php-ext-enable xdebug && \
+  echo "" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini && \
+  echo "xdebug.mode = debug" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini && \
   echo "xdebug.start_with_request = 1" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
 RUN mkdir ./app
 WORKDIR /app
