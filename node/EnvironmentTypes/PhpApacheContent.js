673e9abeb654fc41687926f371b9d6b7b4d5fd98
Danilo Silva <danilocgsilva@gmail.com>
2023-08-03 16:42:54 -0300

    Added update to have the updated repos

--- node/EnvironmentTypes/PhpApacheContent.js
+++ node/EnvironmentTypes/PhpApacheContent.js
@@ -64,6 +64,7 @@ export default class PhpApacheContent extends ContentAbstract {
   getDockerfileContent() {
     const dockerfileContent = `FROM php:8.2-apache-bullseye
 
+RUN apt-get update
 RUN apt-get install vim zip -y
 RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin/ --filename=composer
 RUN pecl install xdebug && docker-php-ext-enable xdebug
